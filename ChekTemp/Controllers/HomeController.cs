28856dc129945389fee33efdb1a0dc226b8d7b82
Dila91 <59875110+Dila91@users.noreply.github.com>
2020-01-23 15:17:07 +0100

    finish

---
+++ ChekTemp/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ChekTemp.Models;
+using ChekTemp.ViewModels;
+
+namespace ChekTemp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            TemperatureViewModel m = new TemperatureViewModel();
+            return View(m);
+        }
+        [HttpPost]
+        public IActionResult Index(TemperatureViewModel m)
+        {
+            m.CheckTemperature();
+            return View(m);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ChekTemp/ViewModels/TemperatureViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChekTemp.ViewModels
+{
+    public class TemperatureViewModel
+    {
+        public TemperatureViewModel()
+        {
+            Message = "";
+        }
+        public string Message { get; set; }
+        public int Temperature { get; set; }
+        public void CheckTemperature()
+        {
+            if (Temperature < 35)
+            {
+                Message = "You might have hypothermia";
+            }
+            if (Temperature > 37)
+            {
+                Message = "You might have a fever";
+            }
+            if (Temperature >=35 && Temperature <= 37)
+            {
+                Message = "You are in perfect health";
+            }
+        }
+    }
+}
